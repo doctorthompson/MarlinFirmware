8d6e6a4fe4a69e76a57260ac06238126c9db005c
Lucas Thompson <doctorthompson@gmail.com>
2020-10-14 14:42:40 -0600

    local config changes

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "bugfix-2.0.x-20201007"
+#define SHORT_BUILD_VERSION "bugfix-2.0.x-20201013"
 
 /**
  * Verbose version identifier which should contain a reference to the location
@@ -41,7 +41,7 @@
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
-#define STRING_DISTRIBUTION_DATE "2020-10-07"
+#define STRING_DISTRIBUTION_DATE "2020-10-13"
 
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
