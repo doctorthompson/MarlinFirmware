a6f886ea7613b86d9c11f3b9f20cc7e901a77c47
Lucas Thompson <doctorthompson@gmail.com>
2021-03-14 05:08:59 -0600

    Bondtech stepper default adjustment

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -797,7 +797,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 440 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 425 }
 
 /**
  * Default Max Feed Rate (mm/s)
